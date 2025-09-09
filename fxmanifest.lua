1988044e5def6270f77b01ba4f65719dc928bcc8
not.skap <exee.ree@gmail.com>
2025-09-09 04:52:09 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,9 @@
 fx_version 'cerulean'
 
-shared_script "@SecureServe/src/module/module.lua"
-shared_script "@SecureServe/src/module/module.js"
-file "@SecureServe/secureserve.key"
 game 'gta5'
 
 author 'not.skap'
-description 'For FluxRP'
+description 'Player licence restricted vehicle model'
 
 shared_scripts {
   '@ox_lib/init.lua',
